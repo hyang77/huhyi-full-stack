7d4e4320c7526dd4b594c5fade37ba7b8e0247f7
Haoli Yang <hyang77@hawk.iit.edu>
2020-06-05 17:19:26 +0800

    create data.js

---
+++ server/data.js
@@ -0,0 +1,19 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "folding chair",
+      category: "chair",
+    },
+    {
+      _id: "2",
+      name: "public line chair",
+      category: "chair",
+    },
+    {
+      _id: "3",
+      name: "office desk",
+      category: "desk",
+    },
+  ],
+};
