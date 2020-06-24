eb5f20bcf2a2572b71513b8f8370a4b85c994005
Haoli Yang <hyang77@hawk.iit.edu>
2020-06-24 17:34:33 +0800

    add handleDelete function

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -8,17 +8,34 @@ function List(props) {
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("http://localhost:3000/api/products");
+      const { data } = await axios.get("http://localhost:4000/api/products");
       setProduct(data);
       console.log(data);
     };
     fetchData();
   }, []);
 
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:4000/api/products/${id}`)
+    .then(() => {
+      // Issue GET request after item deleted to get updated list
+      // that excludes user of id
+      return axios.get("http://localhost:4000/api/products")
+    })
+    .then((res) => {
+      // Update users in state as per-usual
+      const data = res.data;
+      console.log(res.data);
+      setProduct(data);
+    })
+  
+  };
+  
+
   return (
-    <React.Fragment> 
+    <React.Fragment>
       <div className="list-container" style={{ margin: "3em" }}>
-       <div className="d-flex justify-content-end mb-4">
+        <div className="d-flex justify-content-end mb-4">
           <AddButton />
         </div>
         <div className="row border border-light text-center">
@@ -29,14 +46,20 @@ function List(props) {
           <div className="col border border-light"></div>
         </div>
         {products.map((product) => (
-          <div div className="row border border-light">
+          <div className="row border border-light">
             <div className="col border border-light">{product._id}</div>
             <div className="col border border-light">{product.name}</div>
             <div className="col border border-light">{product.category}</div>
             <div className="col border border-light">{product.description}</div>
             <div className="col border border-light">
               <EditButton />
-              <DeleteButton />
+              <button
+                type="button"
+                className="btn btn-danger btn-sm"
+                onClick={(e) => handleDelete(product._id)}
+              >
+                <i className="fa fa-trash-o" aria-hidden="true"></i>
+              </button>
             </div>
           </div>
         ))}
